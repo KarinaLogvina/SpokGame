07cef8637fcd33f89a6a30b2bd65a70f339c6b71
KarinaLogvina <karinalogvina0@gmail.com>
2021-07-26 11:12:52 +0300

    add solution

---
+++ Spok/Spok/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] gameItems = args;
+            bool result = ValidationItems(gameItems);
+            Random rnd = new Random();
+            if(result == true)
+            {
+                List<string> variables = new List<string>(gameItems);
+                int computerMove = rnd.Next(0, variables.Count - 1);
+                byte[] bkeys = getKey();
+                string key = getHash(variables[computerMove].ToString(), bkeys);
+                Console.WriteLine($"HMAC: {key}");
+                string move = ValidationMove(variables);
+                if (move == "0")
+                    return;
+                Game(int.Parse(move), computerMove, variables);
+                Console.WriteLine($"HMAC KEY:{BitConverter.ToString(bkeys).Replace("-", string.Empty).ToUpper()}");
+            }
+        }
+        static bool ValidationItems(string[] gameItems)
+        {
+            var unicItems = new HashSet<string>(gameItems);
+                if (gameItems.Length < 3)
+                {
+                    Console.WriteLine("Wrong input, please write more unless 3 parameters. Try this: 1 2 4 5 6 7 8");
+                    return false;
+                }
+                if (gameItems.Length % 2 == 0)
+                {
+                    Console.WriteLine("Wrong! Enter an odd number of parameters.");
+                    return false;
+                }
+                if (unicItems.Count != gameItems.Length)
+                {
+                    Console.WriteLine("Wrong input, please write unic items. Try this: 1 2 4 5 6 7 8");
+                    return false;
+                }
+                if (gameItems.Length == 0)
+                {
+                    Console.WriteLine("Wrong input, please write something. Try this: 1 2 4 5 6 7 8");
+                    return false;
+                }
+                return true;
+        }
+        static string ValidationMove(List<string> variables)
+        {
+            Console.WriteLine("Available moves:");
+            foreach(string item in variables)
+            {
+                Console.WriteLine($"{variables.IndexOf(item) + 1} - {item}");
+            }
+            Console.WriteLine("0 - exit");
+            Console.Write("Enter your move: ");
+            string move = Console.ReadLine();
+            if (move == "0")
+                return "0";
+            bool result = false;
+            while(!result)
+            {
+                if(int.Parse(move) > variables.Count)
+                {
+                    Console.WriteLine("Wrong move, please be more attentively!");
+                    Console.Write("Enter your move: ");
+                    result = false;
+                    move = Console.ReadLine();
+                }
+                else
+                {
+                    result = true;
+                }
+            }
+            Console.WriteLine($"Your move: {variables[int.Parse(move) - 1]}");
+            return move;
+        }
+        static void Game (int playerMove, int enemyMove, List<string> variables)
+        {
+            int computerMove = enemyMove;
+            Console.WriteLine($"Combuter move: {variables[computerMove]}");
+            List<string> wins= new List<string>();
+            var i = playerMove + 1;
+            var count = 0;
+            bool result = true;
+            int winsLength = (variables.Count - 1) / 2;
+            if (computerMove == playerMove)
+            {
+                Console.WriteLine("It's a draw!");
+                return;
+            }
+            while (count < winsLength)
+            {
+                count++;
+                if (i >= variables.Count)
+                    i = 0;
+                wins.Add(variables[i]);
+                i++;
+            }
+            foreach(string item in wins)
+            {
+                if(variables[computerMove] == item)
+                {
+                    Console.WriteLine("You lose!");
+                    result = false;
+                }
+            }
+            if(result == true)
+            {
+                Console.WriteLine("You Win!");
+            }
+        }
+        static byte[] getKey()
+        {
+            byte[] buffer = new byte[16];
+            using(var rng = RandomNumberGenerator.Create())
+            {
+                rng.GetBytes(buffer);
+            }
+            return buffer;
+        }
+
+        static string getHash (string str, byte[] key)
+        {
+           using (var hmac = new HMACSHA256(getKey()))
+            {
+                byte[] bstr = Encoding.Default.GetBytes(str);
+                var bhash = hmac.ComputeHash(bstr);
+                return BitConverter.ToString(bhash).Replace("-", string.Empty).ToUpper();
+            }
+        }
+    }
+}
